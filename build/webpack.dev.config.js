b29542b585c27b84a9df4c3f87e7037c83153172
stone <stone.grandia@163.com>
2018-02-16 20:03:22 +0800

    修改配置

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -1,13 +1,29 @@
+const webpack = require('webpack');
 const merge = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const baseWebpackConfig = require('./webpack.base.config');
 const util = require('./util');
 
 module.exports = merge(baseWebpackConfig, {
-    entry: [util.resolve('test/main.js')],
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: util.resolve('test/index.html')
-        })
-    ]
+  entry: [util.resolve('test/main.js')],
+  devServer: {
+    hot: true,
+    overlay: true,
+    stats: "errors-only",
+    compress: true,
+    inline: true,
+    port: 8080,
+    open: true,
+    watchOptions: {
+      poll: false
+    }
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new HtmlWebpackPlugin({
+      template: util.resolve('test/index.html')
+    })
+  ]
 });
+
